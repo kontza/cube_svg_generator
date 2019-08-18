ecd42a535539d8221c68fc69476705b94be991e1
Rumpu-Jussi <kontza@gmail.com>
2019-08-18 20:36:15 +0300

    Added title and sequence.

--- generator.py
+++ generator.py
@@ -22,13 +22,17 @@ if __name__ == "__main__":
         color_def = yaml.load(source_yml, Loader=yaml.FullLoader)
         with open("rubiks_cube.svg") as fp:
             soup = BeautifulSoup(fp, "xml")
-            for side in color_def["cube"].keys():
+            for side in color_def["cube"]["faces"].keys():
                 counter = 1
-                for piece in color_def["cube"][side]:
+                for piece in color_def["cube"]["faces"][side]:
                     found_piece = soup.find(id="{}-{}".format(side, counter))
                     if len(piece) == 1:
                         found_piece["fill"] = color_mapping[piece]
                     else:
                         found_piece["fill"] = piece
                     counter += 1
+            text_element = soup.find(id="title")
+            text_element.string = color_def["cube"]["title"]
+            text_element = soup.find(id="sequence")
+            text_element.string = color_def["cube"]["sequence"]
             print(soup.prettify().replace("xmlns:=", "xmlns="))
